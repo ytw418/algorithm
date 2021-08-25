a47747eb8f54e20e3c0fea98474bd66ead6204ec
ytw418 <ytw418@gmail.com>
2021-08-25 13:14:31 +0900

    구구단

---
+++ for1.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+let input = [];
+
+rl.on('line',function(line){
+    input = line.split(' ');
+
+    const num1 = Number(input[0]);
+
+    for(let i = 1; i <= 9; i++)
+    {
+        console.log(`${num1} * ${i} = ${num1 * i}`);
+    }
+
+}).on('close',function(){
+    process.exit();
+});
